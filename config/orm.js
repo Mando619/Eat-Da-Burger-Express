bcbfd19951107f57ff7d98c59cd38785a12fe07b
Armando Estrada <libryum33@hotmail.com>
2020-09-30 18:26:53 -0700

    deleted code didnt need

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,7 @@ function printQuestionMarks(num) {
     var array = [];
   
     for (var i = 0; i < num; i++) {
-      array.push("?");
+      array.push("");
     }
   
     return array.toString();
@@ -17,21 +17,15 @@ function printQuestionMarks(num) {
   
     // loop through the keys and push the key/value as a string int arr
     for (var key in object) {
-      var value = object[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(object, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
+      
         // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
         // e.g. {sleepy: true} => ["sleepy=true"]
-        array.push(key + "=" + value);
+        array.push(key + "=" + object[key]);
       }
-    }
+
   
     // translate array of strings to a single comma-separated string
-    return arr.toString();
+    return array.toString();
   }
   var orm = {
     all: function(tableInput, callBack) {
@@ -83,6 +77,6 @@ function printQuestionMarks(num) {
     }
   };
   
-  // Export the orm object for the model (cat.js).
+  // Export the orm object for the model (burger.js).
   module.exports = orm;
   
--- public/BurgersJQ.js
+++ public/BurgersJQ.js
@@ -1,9 +1,21 @@
-//$("#btn btn-primary").on("submit", function(event) {
-   // event.preventDefault();
+$("#btn btn-primary").on("submit", function(event) {
+    event.preventDefault();
 
-  //  var newBurger = {
-  //      name: $("#burger_id").val().trim(),
+   var newBurger = {
+        name: $("#burger_id").val().trim(),
         
- //   }
+    }
 
-//});
+});
+
+$("#bt btn-default").on('click', function() {
+   const burgerId = $(this.data('id'):
+
+   $.ajax(`/api/burgers/${burgerId}`, {
+      type: "DELETE"
+   })
+   .then(function() {
+      location.reload()
+   })
+});
+
